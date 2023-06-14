0f01ea73dec411ce94a9a10ac19dccd890b6aff1
Bidhan Poudel <bidhanpoudel0@gmail.com>
2023-06-14 18:08:51 +0545

    First update

---
+++ lib/quiz.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState(){
+    return _QuizState();
+  }
+}
+
+
+class _QuizState extends State<Quiz>{
+
+
+  Widget?activeScreen;
+
+  @override
+  void initState() {
+   activeScreen=StartScreen(switchScreen);
+   super.initState(); 
+  }
+
+  void switchScreen(){
+    setState(() {
+      activeScreen= const QuestionScreen();
+    });
+  }
+@override
+
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.redAccent,
+                Colors.purpleAccent,
+              ],
+              begin: Alignment.bottomLeft,
+              end:Alignment.topRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+
+}
